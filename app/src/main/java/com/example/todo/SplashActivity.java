8617bebf631e8bb0fc6cd9b4ed600834db1dbb5f
ParmarGaurav07 <gauravparmar532000@gmail.com>
2024-05-30 15:51:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todo/SplashActivity.java
@@ -0,0 +1,47 @@
+
+package com.example.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+//        getSupportActionBar().hide();
+        ActionBar actionBar = getSupportActionBar();
+
+        // Check if action bar is not null before hiding it
+        if (actionBar != null) {
+            actionBar.hide();
+        } else {
+            Log.e("SplashActivity", "Action bar is null, unable to hide it");
+        }
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
